a475afee190384f413f3bc3ac5d589055acc8192
flavio.miyaji <flavio.miyaji@shx.com.br>
2020-10-12 17:52:25 -0300

    Creating OrphanagesMap page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
 
 import '../styles/pages/orphanages-map.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
 
 const OrphanagesMap = () => {
     return (
         <div id="page-orphanages-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy " />
+                    <h2>Choose an orphanage on the map</h2>
+                    <p>So many children are waiting for a visit.</p>
+                </header>
+                <footer>
+                    <strong>Matão</strong>
+                    <span>São Paulo - SP</span>
+                </footer>
+            </aside>
             <div className="content-wrapper">
                 <h1>Orphanages Map</h1>
             </div>
+            <Link to="" className="create-orphanage">
+                <FiPlus size={26} color="rgba(0, 0, 0, 0.6)" />
+            </Link>
         </div>
     );
 };
