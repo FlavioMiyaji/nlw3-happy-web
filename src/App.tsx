0b048cb2c8597709b143de8450c82dfc669e4bc6
flavio.miyaji <flavio.miyaji@shx.com.br>
2020-10-12 17:14:26 -0300

    Creating Happy landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
 
-interface TitleProps{
-  title: string;
-}
-
-function Title({ title }: TitleProps) {
-  return (
-    <h1>{title}</h1>
-  )
-}
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <Title title="Hello NLW #3" />
-      <Title title="Title 2" />
-      <Title title="Title 3" />
-      <Title title="Title 4" />
+    <div className="App" id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Bring happiness to the world</h1>
+          <p>visit orphanages and change many children's day.</p>
+        </main>
+
+        <div className="location">
+          <strong>Matão</strong>
+          <span>São Paulo - SP</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
